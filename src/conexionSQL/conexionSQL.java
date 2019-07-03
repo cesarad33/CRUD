6aebd13c1120491119ee2703eaf483baa499de5c
Cesar <Cesar@DESKTOP-T9AJDB6>
2019-07-03 01:36:50 -0500

    Probando primer repositorio

---
+++ src/conexionSQL/conexionSQL.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cesar
+ */
+public class conexionSQL {
+    
+    Connection conectar=null;
+    
+    public Connection conexion()
+    {
+       
+        try { 
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:33065/database","root","");
+        
+            JOptionPane.showMessageDialog(null,"conexion exitosa");
+        
+            
+        } catch (Exception e ){
+            
+            JOptionPane.showMessageDialog(null,"error de conexion"+ e.getMessage());
+            
+            
+    }
+    return conectar;
+    } 
+}
